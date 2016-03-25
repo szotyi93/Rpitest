2b8f9d024b02f40538105c6c366b0990e8387d0a
szotyi93 <mirktamas@gmail.com>
2016-03-25 21:48:27 +0100

    Update httppost.py

--- httppost.py
+++ httppost.py
@@ -4,6 +4,10 @@ response = os.system("ping -c 1 " + hostname)
 
 #and then check the response...
 if response == 0:
-  print hostname, 'is up!'
+  print hostname, 'is up!' \
+  import urllib.request \
+  urllib.request.urlopen("https://api.thingspeak.com/update?api_key=O23H5POLXR50IEU2&field1=1").read()
 else:
-  print hostname, 'is down!'
+  print hostname, 'is down!' \
+  import urllib.request \
+  urllib.request.urlopen("https://api.thingspeak.com/update?api_key=O23H5POLXR50IEU2&field1=0").read()
